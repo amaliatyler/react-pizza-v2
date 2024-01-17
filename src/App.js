1ffd1ff0ba4e98ca4f4eb9a340c1444faff30368
amaliatyler <amaliatyler@gmail.com>
2024-01-17 18:16:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 import logo from './logo.svg';
+
 import './App.css';
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Маргарита" price="395" />
+            <PizzaBlock title="Четыре сыра" price="455" />
+            <PizzaBlock title="Маргарита" price="395" />
+            <PizzaBlock title="Маргарита" price="395" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
